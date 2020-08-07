91b06d8a283c1ac2deeedcf329b55cd54a720815
raresmigea <raresini@yahoo.com>
2020-08-07 23:42:23 +0300

    changed app name

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React
+          Chat application
         </a>
       </header>
     </div>
