810a61a448479779fa0b6a1dd55ee8827eaa066c
raresmigea <raresini@yahoo.com>
2020-08-18 23:49:31 +0300

    updated app

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
+        <a className='App-link' href='https://reactjs.org'>
           Chat App
         </a>
       </header>
