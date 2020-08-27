bef66e7c430b294a990182cff42ea376ae54834b
raresmigea <raresini@yahoo.com>
2020-08-28 00:11:39 +0300

    Updated App function

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className='App'>
       <header className='App-header'>
@@ -12,5 +12,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
